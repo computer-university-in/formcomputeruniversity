deb6c42809d7d1df9edad9dea486889b78fd0fb5
Ubuntu <ubuntu@ip-172-31-34-34.ap-south-1.compute.internal>
2020-06-12 08:35:01 +0000

    Fifth CommIT

--- app/models/wdtmz.rb
+++ app/models/wdtmz.rb
@@ -1,5 +1,5 @@
 class Wdtmz < ActiveRecord::Base
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } , uniqueness: { value: true , message: ":You have already filled the form, Thank you so much!" }
   validates :number, :presence => true,
     :numericality => true,:length => { :minimum => 10, :maximum =>15 }
 end
